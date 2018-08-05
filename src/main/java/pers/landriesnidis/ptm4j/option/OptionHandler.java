1d17d258eb19c71bf57bb89c383e84928002dc12
Guijie Lee <landriesnidis@yeah.net>
2018-08-05 15:05:53 +0800

    更新选择项预处理程序块参数项及名称

--- src/main/java/pers/landriesnidis/ptm4j/option/OptionHandler.java
+++ src/main/java/pers/landriesnidis/ptm4j/option/OptionHandler.java
@@ -1,5 +1,11 @@
 package pers.landriesnidis.ptm4j.option;
 
 public interface OptionHandler {
-	void handle(String text);
+	/**
+	 * 执行前的预处理程序
+	 * @param text 接收到文本信息
+	 * @param optionContext 正在执行的选择项对象
+	 * @return 返回true则继续执行选项的触发事件，返回false则不会触发选项的事件
+	 */
+	boolean preparatoryExecuteHandle(String text,Option optionContext);
 }
